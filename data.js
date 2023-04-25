291b11c1c0369ea5f2a48c8182169fbf7b81dfb4
AlexBuck1 <alexcbuck1@gmail.com>
2023-04-25 12:13:41 -0700

    minor changes to get release date to show

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 let albums = [
-    {"name": "The House is Burning", "artist": "Isaiah Rashad", "label": "Top dawg Entertainment", "release date": "July 30, 2021"},
-    {"name": "Astroworld", "artist": "Travis Scott", "label": "Cactus Jack/Epic/Grand Hustle", "release date": "August 3, 2018"},
-    {"name": "2001", "artist": "Dr. Dre", "label": "Aftermath Entertainment/Interscope Records", "release date": "November 16, 1999"},
-    {"name": "HEROES & VILLAINS", "artist": "Metro Boomin", "label": "Boominati Worldwide/Republic Records", "release date": "December 2, 2022"},
-    {"name": "KOD", "artist": "J. Cole", "label": "Dreamville/Roc Nation Records", "release date": "April 20, 2018"}
+    {"name": "The House is Burning", "artist": "Isaiah Rashad", "label": "Top Dawg Entertainment", "releaseDate": "July 30, 2021"},
+    {"name": "Astroworld", "artist": "Travis Scott", "label": "Cactus Jack/Epic/Grand Hustle", "releaseDate": "August 3, 2018"},
+    {"name": "2001", "artist": "Dr. Dre", "label": "Aftermath Entertainment/Interscope Records", "releaseDate": "November 16, 1999"},
+    {"name": "HEROES & VILLAINS", "artist": "Metro Boomin", "label": "Boominati Worldwide/Republic Records", "releaseDate": "December 2, 2022"},
+    {"name": "KOD", "artist": "J. Cole", "label": "Dreamville/Roc Nation Records", "releaseDate": "April 20, 2018"}
 
 ]
 
